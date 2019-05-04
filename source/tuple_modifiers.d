cd30872e05f904a093cca67011f7392c39005c4d
Victor Porton <porton@narod.ru>
2019-05-04 23:02:27 +0300

    initial (working) version

---
+++ source/tuple_modifiers.d
@@ -0,0 +1,20 @@
+module tuple_modifiers;
+
+import std.meta : staticMap;
+
+template addTupleModifiers(string modifiers, T...) {
+    private template addModifier(T1) {
+        mixin("alias addModifier = " ~ modifiers ~ ' ' ~ T1.stringof ~ ';');
+    }
+
+    alias addTupleModifiers = staticMap!(addModifier, T);
+}
+
+unittest {
+    import std.meta;
+
+    alias t = AliasSeq!(int, float);
+    //alias tc = const(t); // does not work in DMD v2.085.1
+    alias tc = addTupleModifiers!("const", t);
+    static assert(is(tc[1] == const float));
+}

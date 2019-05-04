81fa3046c614bc33f428a93305b989724c14a65a
Victor Porton <porton@narod.ru>
2019-05-04 23:04:46 +0300

    added documentation comments

--- source/tuple_modifiers.d
+++ source/tuple_modifiers.d
@@ -2,6 +2,15 @@ module tuple_modifiers;
 
 import std.meta : staticMap;
 
+/**
+Add modifiers `modifiers` to each element of tuple `T`.
+
+Example:
+```
+alias t = AliasSeq!(int, float);
+alias tc = addTupleModifiers!("const shared", t);
+static assert(is(tc[1] == const shared float));```
+*/
 template addTupleModifiers(string modifiers, T...) {
     private template addModifier(T1) {
         mixin("alias addModifier = " ~ modifiers ~ ' ' ~ T1.stringof ~ ';');
